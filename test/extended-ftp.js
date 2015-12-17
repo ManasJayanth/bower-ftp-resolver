1dc3f5128b53fc2096f72333fb497ad0eba814fc
Manas <prometheansacrifice@gmail.com>
2015-12-17 19:06:21 +0530

    Adds tests for `extended-ftp.js`

--- test/extended-ftp.js
+++ test/extended-ftp.js
@@ -48,5 +48,45 @@ describe('FTP', function () {
                 }
             });
         });
+        it('rejects when invalid source is specified', function (done) {
+            var extendedFTP = new ExtendedFTP();
+            var tempDir = 'test-temp-dir-' + Math.floor(Math.random() * 1000),
+                testText = 'testing-text',
+                testFileName = 'test.file';
+
+            extendedFTP.on('ready', function () {
+                extendedFTP.copyDirAsync('/invalid-source', '.')
+                    .then(function () {
+                        var textFound = fs.readFileSync('./' + tempDir + '/' +
+                                                        testFileName).toString();
+                        expect(textFound, testText);
+                        fs.unlinkSync('./' + tempDir + '/' + testFileName);
+                        fs.rmdir(tempDir);
+                        done();
+                    })
+                    .catch(function (error) {
+                        expect(error).to.have.property('code');
+                        expect(error.code).to.equal(550);
+                        expect(error).to.have.property('isOperational');
+                        expect(error.code).to.be.ok();
+                        expect(error).to.have.property('cause');
+                        expect(error.cause).to.have.property('message');
+                        expect(error.cause.message).to.equal('Directory not found.');
+                        expect(error.cause).to.have.property('message');
+                        expect(error.cause.code).to.equal(550);
+                        done();
+                    });
+            });
+
+            fs.mkdir(tempDir);
+            fs.writeFile('./' + tempDir + '/' + testFileName, testText, function (err) {
+                if (!err) {
+                    extendedFTP.connect({
+                        host: 'localhost',
+                        port: __FTP_PORT__
+                    });
+                }
+            });
+        });
     });
 });
